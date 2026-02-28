11010b96041b0f9739289ec2c1e5ff9df784f29f
Daniel Cazorla <dcazorla.0190@gmail.com>
2026-02-28 14:58:53 -0400

    cambios

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -3,7 +3,7 @@
     class="w-full flex justify-center items-center cont-primary bg-gray-100"
   >
     <div
-      class="flex flex-row w-full justify-items-start items-center gap-3 py-5 px-10"
+      class="flex flex-row w-full justify-items-start items-center gap-3 py-5 px-5 md:px-10"
     >
       <i class="bi bi-wallet text-xl text-white text-shadow-2xs text-shadow-gray-700 py-1 px-2 rounded-lg bg-(--rosa)"></i>
       <h1 class="text-gray-700 poppins-bold text-[13px] md:text-2xl">EventBudget-4370</h1>
